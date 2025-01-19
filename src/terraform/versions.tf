d8a1a1150c56b71ffa85403b227d8c10173cd86c
Mark Tinderholt <markti@outlook.com>
2025-01-19 17:58:06 -0500

    Managed by Terraform

---
+++ src/terraform/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.10.0"
+    }
+  }
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
