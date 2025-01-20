1834e2167dcf6344407ed9b1160d9b924a7e87a5
Mark Tinderholt <markti@outlook.com>
2025-01-19 21:35:44 -0500

    Managed by Terraform

---
+++ src/terraform/network.tf
@@ -0,0 +1,8 @@
+resource "azurerm_virtual_network" "main" {
+
+  name                = "vnet-${var.application_name}-${var.environment_name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = [var.base_address_space]
+
+}
