d9989ff1b5252c73b225e838e8381d69ba5e9d7e
Mark Tinderholt <markti@outlook.com>
2025-01-19 21:52:28 -0500

    Managed by Terraform

--- src/terraform/compute-vm1-ssh.tf
+++ src/terraform/compute-vm1-ssh.tf
@@ -7,7 +7,7 @@ resource "azurerm_key_vault_secret" "vm1_ssh_private" {
 
   name         = "vm1-ssh-private"
   value        = tls_private_key.vm1.private_key_pem
-  key_vault_id = data.azurerm_key_vault.main.id
+  key_vault_id = azurerm_key_vault.main.id
 
   depends_on = [azurerm_role_assignment.terraform_user]
 }
@@ -16,7 +16,7 @@ resource "azurerm_key_vault_secret" "vm1_ssh_public" {
 
   name         = "vm1-ssh-public"
   value        = tls_private_key.vm1.public_key_openssh
-  key_vault_id = data.azurerm_key_vault.main.id
+  key_vault_id = azurerm_key_vault.main.id
 
   depends_on = [azurerm_role_assignment.terraform_user]
 }
