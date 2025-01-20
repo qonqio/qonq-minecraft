64818a6e60731c02db45fb6bdcf66b7df79a353b
Mark Tinderholt <markti@outlook.com>
2025-01-19 21:49:30 -0500

    Managed by Terraform

---
+++ src/terraform/keyvault.tf
@@ -0,0 +1,16 @@
+resource "azurerm_key_vault" "main" {
+
+  name                      = "kv-${var.application_name}-${var.environment_name}"
+  location                  = azurerm_resource_group.main.location
+  resource_group_name       = azurerm_resource_group.main.name
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  sku_name                  = "standard"
+  enable_rbac_authorization = true
+
+}
+
+resource "azurerm_role_assignment" "terraform_user" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
