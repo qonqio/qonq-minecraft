3251a97cf45156c0560962a3916a64b6feb2b987
Mark Tinderholt <markti@outlook.com>
2025-01-19 21:35:45 -0500

    Managed by Terraform

---
+++ src/terraform/network-subnets.tf
@@ -0,0 +1,9 @@
+locals {
+  default_address_space = cidrsubnet(var.base_address_space, 2, 1)
+}
+resource "azurerm_subnet" "default" {
+  name                 = "snet-default"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [local.bravo_address_space]
+}
