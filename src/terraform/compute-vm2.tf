b1356172b6b6ad518c359dbba2430f8d32c8094a
markti <markti@outlook.com>
2025-07-23 13:39:19 -0400

    v6

--- src/terraform/compute-vm2.tf
+++ src/terraform/compute-vm2.tf
@@ -21,7 +21,7 @@ resource "azurerm_network_interface" "vm2" {
 }
 
 data "azurerm_shared_image_version" "vm2" {
-  name                = "2025.07.4"
+  name                = "2025.07.6"
   image_name          = "ubuntu-minecraft-java"
   gallery_name        = var.azure_gallery_name
   resource_group_name = var.azure_gallery_resource_group
